6fcae9c4272bdffe1bae341c297ffeac75bf8768
Asri <asri>
2026-07-25 11:53:18 +0530

    eks deployment

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,20 +1,21 @@
 data "aws_ami" "joindevops" {
-    owners           = ["526426842890"]
-    most_recent      = true
-    
-   filter {
-  name   = "name"
-  values = ["Redhat-9-DevOps-Practice"]
-}
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+  owners      = ["973714476881"]
+  most_recent = true
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
