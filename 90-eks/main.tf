b301f54a918dbb9338d81f93a9184b79e5db0dee
Asri <asri>
2026-07-25 13:06:34 +0530

    eks deployment

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -1,86 +1,152 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0" # this is module version
+  version = "~> 21.0"
+
+  ##################################################
+  # EKS CLUSTER
+  ##################################################
+  # Kubernetes Upgrade Strategy
+  #
+  # Current Kubernetes Version : 1.32
+  # Target Kubernetes Version  : 1.33
+  #
+  # Note:
+  # EKS Platform Version (eks.xx) is managed
+  # automatically by AWS and cannot be configured
+  # in Terraform.
+  ##################################################
 
   name               = local.common_name_suffix
-  # kubernetes_version = "1.33"
   kubernetes_version = var.eks_version
 
+  ##################################################
+  # EKS ADD-ONS
+  ##################################################
+
   addons = {
-    coredns                = {}
+    coredns = {}
+
     eks-pod-identity-agent = {
       before_compute = true
     }
-    kube-proxy             = {}
-    vpc-cni                = {
+
+    kube-proxy = {}
+
+    vpc-cni = {
       before_compute = true
     }
+
     metrics-server = {}
   }
 
-  endpoint_public_access = false
+  ##################################################
+  # CLUSTER CONFIGURATION
+  ##################################################
+
+  endpoint_public_access                   = false
   enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = local.vpc_id
   subnet_ids               = local.private_subnet_ids
   control_plane_subnet_ids = local.private_subnet_ids
+
   create_node_security_group = false
   create_security_group      = false
-  node_security_group_id = local.eks_node_sg_id
-  security_group_id = local.eks_control_plane_sg_id
 
-  # EKS Managed Node Group(s)
+  node_security_group_id = local.eks_node_sg_id
+  security_group_id      = local.eks_control_plane_sg_id
+
+  ##################################################
+  # EKS MANAGED NODE GROUPS
+  ##################################################
+
   eks_managed_node_groups = {
+
+    ##################################################
+    # BLUE NODE GROUP (CURRENT PRODUCTION)
+    ##################################################
+    #
+    # Kubernetes Version : 1.32
+    #
+    # Production workloads are currently running here.
+    ##################################################
+
     blue = {
-      create = var.enable_blue
-      ami_type       = "AL2023_x86_64_STANDARD"
+      create             = var.enable_blue
+      ami_type           = "AL2023_x86_64_STANDARD"
       kubernetes_version = var.eks_nodegroup_blue_version
+
       instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
+
+      min_size     = 2
+      desired_size = 2
+      max_size     = 10
+
+      iam_role_additional_policies = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
       }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
 
       labels = {
         nodegroup = "blue"
       }
     }
 
+    ##################################################
+    # GREEN NODE GROUP (UPGRADE TARGET)
+    ##################################################
+    #
+    # Step 1:
+    # Create Green Node Group with Kubernetes 1.32
+    #
+    # Step 2:
+    # Upgrade Control Plane from 1.32 -> 1.33
+    #
+    # Step 3:
+    # Upgrade Green Node Group to Kubernetes 1.33
+    #
+    # Step 4:
+    # Validate Green Environment
+    #
+    # Step 5:
+    # Remove NoSchedule taint
+    #
+    # Step 6:
+    # Cordon & Drain Blue Nodes
+    #
+    # Step 7:
+    # Kubernetes recreates the application Pods
+    # on the Green Node Group.
+    #
+    # Step 8:
+    # Delete the Blue Node Group.
+    ##################################################
+
     green = {
-      create = var.enable_green
-      ami_type       = "AL2023_x86_64_STANDARD"
+      create             = var.enable_green
+      ami_type           = "AL2023_x86_64_STANDARD"
       kubernetes_version = var.eks_nodegroup_green_version
+
       instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
+
+      min_size     = 2
+      desired_size = 2
+      max_size     = 10
+
+      iam_role_additional_policies = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
         amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
       }
-      
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
 
-      # taints = {
-      #   upgrade = {
-      #     key = "upgrade"
-      #     value = "true"
-      #     effect = "NO_SCHEDULE"
-      #   }
-      # }
+      # Prevent workloads from scheduling
+      # until validation is completed.
+      taints = {
+        upgrade = {
+          key    = "upgrade"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        }
+      }
 
       labels = {
         nodegroup = "green"
@@ -88,11 +154,14 @@ module "eks" {
     }
   }
 
+  ##################################################
+  # TAGS
+  ##################################################
+
   tags = merge(
     local.common_tags,
     {
-        Name = local.common_name_suffix
+      Name = local.common_name_suffix
     }
   )
-  
 }
--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -1,35 +1,72 @@
+##################################################
+# Project Details
+##################################################
+
 variable "project_name" {
-    default = "roboshop"
+  type        = string
+  description = "Project Name"
+  default     = "roboshop"
 }
 
 variable "environment" {
-    default = "dev"
+  type        = string
+  description = "Environment Name"
+  default     = "dev"
 }
 
 variable "zone_id" {
-    default = "Z0883794XJ2EA3764V8N"
+  type        = string
+  description = "Route53 Hosted Zone ID"
+  default     = "Z0883794XJ2EA3764V8N"
 }
 
 variable "domain_name" {
-    default = "eswar.fun"
+  type        = string
+  description = "Route53 Domain Name"
+  default     = "eswar.fun"
 }
+
+##################################################
+# EKS Cluster Version
+##################################################
 
 variable "eks_version" {
-
+  type        = string
+  description = "EKS Control Plane Kubernetes Version"
 }
+
+##################################################
+# Blue Node Group Version
+##################################################
 
 variable "eks_nodegroup_blue_version" {
-
+  type        = string
+  description = "Blue Node Group Kubernetes Version"
 }
+
+##################################################
+# Green Node Group Version
+##################################################
 
 variable "eks_nodegroup_green_version" {
-
+  type        = string
+  description = "Green Node Group Kubernetes Version"
 }
 
-variable enable_blue {
+##################################################
+# Blue Node Group Enable/Disable
+##################################################
 
+variable "enable_blue" {
+  type        = bool
+  description = "Enable Blue Node Group"
 }
 
-variable enable_green {
-    
+##################################################
+# Green Node Group Enable/Disable
+##################################################
+
+variable "enable_green" {
+  type        = bool
+  description = "Enable Green Node Group"
 }
